c7b7c6a7f6d477f70a0aee8d52ce2c8e80dafd39
Ander <ibysytho@gmail.com>
2021-02-11 22:21:10 +0100

    fixing routes

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Give permission for everything in the sharerecipes directory *
-sudo chmod -R 777 /home/ec2-user/sharerecipes
+sudo chmod -R 777 ~/sharerecipes
 
-# Navigate into our working directory where we have all our github files
-cd /home/ec2-user/sharerecipes
+# Add npm and node to path // Install pm2
 
-# Add npm and node to path
+cd ~/sharerecipes
 export NVM_DIR="$HOME/.nvm"	
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
@@ -18,11 +17,10 @@ nvm alias default $(node -v)
 sudo ln -s ~/.nvm/versions/node/$(node -v)/bin/node /usr/bin/node
 sudo ln -s ~/.nvm/versions/node/$(node -v)/bin/npm /usr/bin/npm
 
-# Install lerna and install deps
-npm install
-npm run initialize
-
 npm install pm2 -g
+
+sudo ln -s ~/.nvm/versions/node/$(node -v)/bin/npm /usr/bin/pm2
+
 # cp env and run build
-cd /home/ec2-user/sharerecipes/packages/frontend
+cd ~/sharerecipes/packages/frontend
 cp example-front.env .env.production.local
--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Run server *
-cd /home/ec2-user/sharerecipes/packages/frontend
+cd ~/sharerecipes/packages/frontend
 pm2 start npm --name "front" -- start
 
-cd ../server
+cd ~/sharerecipes/packages/server
 pm2 start npm --name "back" -- start
