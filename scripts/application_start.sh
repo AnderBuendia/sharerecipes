6a3873fcad0689df65e0c75b19f9bb8219337414
Ander <ibysytho@gmail.com>
2021-02-12 14:00:20 +0100

    appspec changes prod

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Run server *
-cd ~/sharerecipes/packages/frontend
-pm2 start npm --name "front" -- start
-
 cd ~/sharerecipes/packages/server
 pm2 start npm --name "back" -- start
+
+cd ~/sharerecipes/packages/frontend
+pm2 start ecosystem.config.js --env production
+
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Stopping existing node servers
 echo "Stopping any existing node server"
-if pgrep -l node
-    then
-    pm2 kill
-    pkill node
-    echo "Ready"
-fi
+
+pm2 kill
+pm2 delete all
+killall node
+
+echo "Ready"
