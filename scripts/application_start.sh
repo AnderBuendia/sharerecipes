83ae5eed94052467c8dfda0badae8ee0b4687a0d
Ander <ibysytho@gmail.com>
2021-02-12 16:34:51 +0100

    fix typo error

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,5 +5,5 @@ cd ~/sharerecipes/packages/server
 /usr/bin/pm2 start /usr/bin/npm --name "back" -- start
 
 cd ~/sharerecipes/packages/frontend
-/usr/bin/pm2 start /usr/bin/npm --name "front" start
+/usr/bin/pm2 start /usr/bin/npm --name "front" -- start
 
