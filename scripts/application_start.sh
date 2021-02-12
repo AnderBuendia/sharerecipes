77173a8fd4e16f32989b7026a333add49a4e9f85
Ander <ibysytho@gmail.com>
2021-02-12 14:40:54 +0100

    test build 9

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Run server *
-#cd ~/sharerecipes/packages/server
-# pm2 start npm --name "back" -- start
+cd ~/sharerecipes/packages/server
+pm2 start npm --name "back" -- start &
 
 cd ~/sharerecipes/packages/frontend
 pm2 start ecosystem.config.js --env production
