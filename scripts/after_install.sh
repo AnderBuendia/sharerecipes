c4eb75f8faf4428e298964f370bc7ed449b7bf68
Ander <ibysytho@gmail.com>
2021-05-12 23:53:49 +0200

    change node version

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -10,9 +10,9 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
-nvm install node
-nvm use node
-nvm alias default $(node -v)
+nvm install 15.13.0
+nvm use 15.13.0
+nvm alias default 15.13.0
 
 npm install
 npm install pm2 -g
