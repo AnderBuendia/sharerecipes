bc0c4bda280dd706d48608d484764e85df1474ab
Ander <ibysytho@gmail.com>
2021-09-09 13:30:52 +0200

    ci: update pm2 command to start server

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # Run server *
-cd ~/sharerecipes/server
-/usr/bin/pm2 start /usr/bin/npm --name "back" -- start
+/usr/bin/pm2 start "npm run start:server" --name "back"
+/usr/bin/pm2 start "npm run start:frontend" --name "frontend"
 
-cd ~/sharerecipes/frontend
-/usr/bin/pm2 start ecosystem.config.js --env production
+# cd ~/sharerecipes/server
+# /usr/bin/pm2 start /usr/bin/npm --name "back" -- start
+
+# cd ~/sharerecipes/frontend
+# /usr/bin/pm2 start ecosystem.config.js --env production
 
