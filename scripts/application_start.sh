41d7efb09ce8613e09ad8e9fe3683c4e2588abdb
Ander <ibysytho@gmail.com>
2021-09-09 16:15:36 +0200

    ci: modify pm2 command to start server

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,8 +2,8 @@
 
 # Run server *
 cd ~/sharerecipes
-/usr/bin/pm2 start "npm run start:server" --name "back"
-/usr/bin/pm2 start "npm run start:frontend" --name "front"
+pm2 start "npm run start:server" --name "back"
+pm2 start "npm run start:frontend" --name "front"
 
 # cd ~/sharerecipes/server
 # /usr/bin/pm2 start /usr/bin/npm --name "back" -- start
