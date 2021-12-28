9fd19079b2860fad24acc59cd288da0e4abfd32d
anderb <ibysytho@gmail.com>
2021-12-28 21:18:03 +0100

    Delete scripts directory

--- scripts/application_start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Run server *
-cd ~/sharerecipes
-/usr/bin/pm2 start "npm run start:server" --name "back"
-
-cd ~/sharerecipes/frontend
-/usr/bin/pm2 start ecosystem.config.js --env production
--- scripts/application_stop.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Stopping existing node servers
-echo "Stopping any existing node server"
-
-pm2 kill
-pm2 delete all
-killall node
-
-echo "Ready"
