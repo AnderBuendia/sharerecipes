d4432139eb12b3663210c94d552245bffba41cfb
Ander <ibysytho@gmail.com>
2022-01-05 19:15:39 +0100

    perf: fonts style in document file

--- frontend/src/components/generic/Head.tsx
+++ frontend/src/components/generic/Head.tsx
@@ -20,14 +20,6 @@ const Head: FC<HeadProps> = ({ title, description, url, noindex = false }) => (
     <link rel="canonical" href={process.env.NEXT_PUBLIC_SITE_URL || '' + url} />
     {noindex && <meta name="robots" content="noindex" />}
 
-    <link
-      href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;400;700&display=swap"
-      rel="stylesheet"
-    />
     <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
     <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
     <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -16,7 +16,16 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Work+Sans:wght@100;400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <div id="my-portal" />
